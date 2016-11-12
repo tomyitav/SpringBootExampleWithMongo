d4d4fe8b257872805869563a0ccd13b66e67a877
unknown <tomyitav@gmail.com>
2016-11-12 13:20:07 +0200

    Deleted java mongo config and changed application.properties

--- src/main/java/com/example/app/SpringMongoApplication.java
+++ src/main/java/com/example/app/SpringMongoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.example")
 @EnableAutoConfiguration
+@EnableMongoRepositories(basePackages = {"com.example"})
 public class SpringMongoApplication {
 
 	public static void main(String[] args) {
