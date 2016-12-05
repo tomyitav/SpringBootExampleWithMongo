f31da521d5e6f12f34c826e18020c7f86a353ea2
Tom <tomyitav@gmail.com>
2016-12-05 16:06:28 +0200

    First support for web sockets

--- src/main/java/com/example/SpringMongoApplication.java
+++ src/main/java/com/example/SpringMongoApplication.java
@@ -1,10 +1,7 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class SpringMongoApplication {
--- src/main/java/com/example/controllers/StubController.java
+++ src/main/java/com/example/controllers/StubController.java
@@ -2,14 +2,39 @@ package com.example.controllers;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/stub")
 public class StubController {
+	
+//	private List<SseEmitter> emmiters = new CopyOnWriteArrayList<>();
+//	
+//	@RequestMapping("/questions")
+//	public SseEmitter questions(){
+//		SseEmitter sseEmitter = new SseEmitter();
+//		emmiters.add(sseEmitter);
+//		sseEmitter.onCompletion(() -> emmiters.remove(sseEmitter));
+//		return sseEmitter;
+//	}
+//	
+//	@RequestMapping(value = "/new-question", method = RequestMethod.POST)
+//	public void postQuestion(String question) {
+//		for(SseEmitter e : emmiters) {
+//			try {
+//				e.send(SseEmitter.event().name("spring").data(question));
+//			} catch (IOException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+//		}
+//	}
+	
 	@RequestMapping(value = "/stublist.json", method = RequestMethod.GET)
     public List<String> getCarList() {
 		List<String> l = Arrays.asList("Hi", "Stub", "Controller");
