a6b5e5ea2df1c8e6aa643ec9f75c47ea55c452a8
Tom <tomyitav@gmail.com>
2016-12-07 12:42:20 +0200

    Back to simple websocket

--- src/main/java/com/example/controllers/QuestionController.java
+++ src/main/java/com/example/controllers/QuestionController.java
@@ -1,16 +1,13 @@
 package com.example.controllers;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.socket.TextMessage;
 
 @Controller
 public class QuestionController {
 
-	@MessageMapping("/hello")
-	@SendTo("/topic/questions")
-	public TextMessage processQuestion(TextMessage question){
+	@MessageMapping("/questions")
+	public String processQuestion(String question){
 		System.out.println("Begin transformation of " + question);
 		return question;
 	}
